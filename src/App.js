cbb50b3e1f6862138d0b4774cbbdbd713e17d289
Nads15 <arshi8838@gmail.com>
2022-12-22 21:28:24 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Thiw is my html h3ading</h1>
+     <lable>Name of Case Study: </lable>
+     <input type="text"></input> <br></br>
+     <lable>Topic: </lable>
+     <input type="text"></input> <br></br>
+     <lable>Sub-topic: </lable>
+     <input type="text"></input> <br></br>
+     <lable>Purpose/Objective: </lable>
+     <input type="text"></input> <br></br>
+     <lable>Overview: </lable>
+     <input type="text"></input> <br></br>
+     <lable>Desired Information: </lable>
+     <input type="text"></input> <br></br>
+     <lable>What is to be studied/found by his study? </lable>
+     <input type="text"></input> <br></br>
+     <lable>What will contributor gain ? </lable>
+     <input type="text"></input> <br></br>
+     <lable>Skill Wise: </lable>
+     <input type="text"></input> <br></br>
+
+    </div>
+  );
+}
+
+export default App;
